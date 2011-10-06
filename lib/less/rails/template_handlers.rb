d732c5392f02f716168d3812f29b086bf7fd3f3f
Thorben Schröder <stillepost@gmail.com>
2011-10-07 01:41:14 +0200

    [DIRTY-HACK] Makes less template handler work again

--- lib/less/rails/template_handlers.rb
+++ lib/less/rails/template_handlers.rb
@@ -1,6 +1,33 @@
 module Less  
   module Rails
     
+    class Tilt::LessTemplate
+
+      def prepare
+      end
+
+      def evaluate(scope, locals, &block)
+        @output ||= begin
+          parser = ::Less::Parser.new less_parser_options(scope)
+          engine = parser.parse(data)
+          engine.to_css
+        end
+      end
+
+      protected
+
+      def less_options_from_rails(scope)
+        scope.environment.context_class.less_config
+      end
+
+      def less_parser_options(scope)
+        less_options = less_options_from_rails(scope)
+        paths = less_options[:paths] + scope.environment.paths
+        {:filename => eval_file, :line => line, :paths => paths.map(&:to_s)}
+      end
+
+    end
+    
     class LessTemplate < Tilt::LessTemplate
       
       TO_CSS_KEYS = [:compress, :optimization, :silent, :color]
